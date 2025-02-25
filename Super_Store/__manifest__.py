31673e4a04cbcc7e2cef2b1a35bc027db013b058
AyaAhmed50 <ayaahmed01552466951@gmail.com>
2025-02-25 22:22:18 +0200

    Initial commit

---
+++ Super_Store/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Super Store",
+    "author": "Teams 4",
+    "category": "Website",
+    "version": "17.0.0.1.0",
+    "depends": ['base'],
+    "data": [],
+    "application": True,
+    "installable": True,
+}
---
+++ Super_Store/models/Store.py
@@ -0,0 +1,9 @@
+# from extra_addons import models, fields
+
+# class Superstore(models.Model):
+#     _name = 'superstore.product'
+#     _description = 'Superstore Product'
+
+#     name = fields.Char(string="Product Name", required=True)
+#     price = fields.Float(string="Price")
+#     stock = fields.Integer(string="Stock Quantity")
