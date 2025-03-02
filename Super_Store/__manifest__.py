adf76a8a04fcb1aedbbb87c14eb39a436984af64
AyaAhmed50 <ayaahmed01552466951@gmail.com>
2025-03-02 22:55:07 +0200

    the updated until video 2 40:00

--- Super_Store/__manifest__.py
+++ Super_Store/__manifest__.py
@@ -1,10 +1,19 @@
 {
     "name": "Super Store",
     "author": "Teams 4",
+    "license": "LGPL-3",
     "category": "Website",
-    "version": "17.0.0.1.0",
+    "version": "17.0.1.0.0",
     "depends": ['base'],
-    "data": [],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/customer_views.xml',
+        'views/menu.xml',
+        
+    ],
+    "images": [
+        'static/description/digital-print-logo-design-vector.jpg',
+    ],
     "application": True,
     "installable": True,
 }
--- Super_Store/models/Store.py
+++ Super_Store/models/Store.py
@@ -1,9 +1,13 @@
-# from extra_addons import models, fields
+from odoo import api, models, fields
 
-# class Superstore(models.Model):
-#     _name = 'superstore.product'
-#     _description = 'Superstore Product'
+class StoreCustomer(models.Model):
+    _name = 'superstore.customer'  # Change to represent customers
+    _description = 'Superstore Customer'
 
-#     name = fields.Char(string="Product Name", required=True)
-#     price = fields.Float(string="Price")
-#     stock = fields.Integer(string="Stock Quantity")
+    name = fields.Char(string="Customer Name", required=True)
+    email = fields.Char(string="Email")
+    phone = fields.Char(string="Phone Number")
+    gender = fields.Selection([("male", 'Male'), ("female", 'Female')], string="Gender")
+    birth_date = fields.Date(string="Date of Birth")
+    address = fields.Text(string="Address")
+    loyalty_points = fields.Integer(string="Loyalty Points", default=0)  # Example of a customer-related field
